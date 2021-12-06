5e6a3ce980ae4976ae5beb4e3750ff8fcabce77c
kimsunhak <kkyj3954@naver.com>
2021-12-06 18:22:44 +0900

    ** FirstCommit

---
+++ js/ui.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    $('#main').fullpage({
+        anchors: ['section1', 'section2', 'section3'],
+        autoScrolling: true,
+        scrollHorizontally: true,
+        controlArrows: false,
+        onLeave: function (section, index) {
+            if (index == 1) {
+                $('#header > div > a > h1').html('Gyuwon').css({color:'#ffffff'}).css({background:'url("../images/icon_home.png") no-repeat left center'});
+            } else if (index == 2) {
+                $('#header > div > a > h1').html('Gyuwon').css({color:'#000000'}).css({background:'url("../images/icon_home_black.png") no-repeat left center'});
+            } else if (index == 3) {
+                $('#header > div > a > h1').html('Gyuwon').css({color:'#ffffff'}).css({background:'url("../images/icon_home.png") no-repeat left center'});
+            }
+        }
+    })
+});
